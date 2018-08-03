8b8a647de2419ad6f0818fc95ded392eddb59346
Unknown <karinaaakos@gmail.com>
2018-08-03 20:44:58 +0200

    Starter HTML & CSS

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" >
+    <title>KOS hadmade - galeria zdjęć</title>
+  </head>
+  <body>
+    <nav>
+    <h2 class="logo">KOS handmade</h2>
+    <div class="menu">
+    <a href="index.php">Home</a>
+    <a href="index.php">Papier</a>
+    <a href="index.php">Drewno</a>
+    </div>
+    <div class="login"><button class="btn btn-dark">Zaloguj</button></div>
+    </nav>
+<section id="posts" class="container">
+<div class="picture">
+<img src="img/background.png" alt="" class="pic">
+<h4>Category</h4>
+<h6>Lorem Ipsum dolores mates sonio del grande</h6>
+</div>
+</section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
